d9ed065f7b3086484c56cf9fc5ea8c6fcba9c4e3
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-05-12 16:30:40 -0700

    Entered info for main.handlebars, changeed the so that you can see the route on in the server, and added a nav folder to the paritals as well as the nav partial file

--- server.js
+++ server.js
@@ -17,13 +17,17 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/catsController.js");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
