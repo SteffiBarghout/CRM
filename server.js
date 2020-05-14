780bc55d6d9f8bf4cbac05271cd55e38c93c1458
Michael <mikeystoddard@gmail.com>
2020-05-14 16:20:28 -0700

    form for login page

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
-
-app.use(routes);
+app.get("/", function (req, res) {
+    res.render("index");
+  });
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
