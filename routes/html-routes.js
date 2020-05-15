8f45059f23aaeeaf7f2637b352982506b4efbb45
hishamss <hishamss90@gmail.com>
2020-05-15 10:13:19 -0700

    fixed authentication functions

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,8 @@
-module.exports = function (app) {
+module.exports = function (
+  app,
+  isAuthenticatedMiddleware,
+  isNotAuthenticatedMiddleware
+) {
   app.get("/", isAuthenticatedMiddleware(), (req, res) => {
     console.log("///////CurrentUser: ", req.user);
     if (req.user === "admin") {
@@ -12,26 +16,30 @@ module.exports = function (app) {
   app.get("/login", isNotAuthenticatedMiddleware(), (req, res) => {
     res.render("login");
   });
+
+  app.get("/settings", isAuthenticatedMiddleware(), (req, res) => {
+    res.render("settings");
+  });
 };
 
-function isAuthenticatedMiddleware() {
-  return (req, res, next) => {
-    console.log(
-      `req.session.passport.user: ${JSON.stringify(req.session.passport)}`
-    );
+// function isAuthenticatedMiddleware() {
+//   return (req, res, next) => {
+//     console.log(
+//       `req.session.passport.user: ${JSON.stringify(req.session.passport)}`
+//     );
 
-    if (req.isAuthenticated()) return next();
-    res.redirect("/login");
-  };
-}
+//     if (req.isAuthenticated()) return next();
+//     res.redirect("/login");
+//   };
+// }
 
-function isNotAuthenticatedMiddleware() {
-  return (req, res, next) => {
-    console.log(
-      `req.session.passport.user: ${JSON.stringify(req.session.passport)}`
-    );
+// function isNotAuthenticatedMiddleware() {
+//   return (req, res, next) => {
+//     console.log(
+//       `req.session.passport.user: ${JSON.stringify(req.session.passport)}`
+//     );
 
-    if (!req.isAuthenticated()) return next();
-    res.redirect("/");
-  };
-}
+//     if (!req.isAuthenticated()) return next();
+//     res.redirect("/");
+//   };
+// }
