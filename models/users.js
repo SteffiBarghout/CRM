ca8aaeaa9d6d4b60afab1c87b7447faa2ed401a8
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-05-23 09:15:12 -0700

    models users.js column updates

--- models/users.js
+++ models/users.js
@@ -1,32 +1,43 @@
-module.exports = function (sequelize, DataTypes) {
-  var Users = sequelize.define(
-    "Users",
-    {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1],
-        },
-      },
-      password: {
-        type: DataTypes.STRING,
-        validate: {
-          len: [1],
-        },
-      },
+module.exports = function(sequelize, DataTypes) {
+    var Users = sequelize.define(
+        "Users", {
+            firstName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            lastName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1],
+                },
+            },
+            password: {
+                type: DataTypes.STRING,
+                validate: {
+                    len: [1],
+                },
+            },
 
-      profImg: {
-        type: DataTypes.STRING,
-        defaultValue:
-          "https://images-test-hss.s3.us-east-2.amazonaws.com/ProfileImgs/default.jpg",
-      },
+            profImg: {
+                type: DataTypes.STRING,
+                defaultValue: "https://images-test-hss.s3.us-east-2.amazonaws.com/ProfileImgs/default.jpg",
+            },
 
-    },
-    {
-      timestamps: false,
-      freezeTableName: true,
-    }
-  );
-  return Users;
+        }, {
+            timestamps: false,
+            freezeTableName: true,
+        }
+    );
+    return Users;
 };
