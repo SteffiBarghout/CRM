e0052362062a89c8a5fba4d097465f4c1f22d9fa
hishamss <hishamss90@gmail.com>
2020-05-24 05:21:25 -0700

    fixed hiding the modal with fade

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-insert into users (firstName, lastName, username, password, email) values ("Steffi", "Wellington", "admin", '$2b$10$5YaHDamMenYbZCYRKOjlxeA8m6GjcIv5q1TH4JmE7lccbPSCHbxUS', "steffisbootcampdrive@gmail.com");
+insert into Users (firstName, lastName, username, password, email) values ("Steffi", "Wellington", "admin", '$2b$10$5YaHDamMenYbZCYRKOjlxeA8m6GjcIv5q1TH4JmE7lccbPSCHbxUS', "steffisbootcampdrive@gmail.com");
 
 insert into users (firstName, lastName, username, password, profImg) 
 values ("Sue", "Smith", "SS", '123', NULL),
